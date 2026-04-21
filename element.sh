83d39b1be056e753555739670f7b8d2e0697110f
SlightlyHappy <104701274+SlightlyHappy@users.noreply.github.com>
2026-04-21 12:47:52 -0400

    chore: this poleaseease

--- element.sh
+++ element.sh
@@ -34,3 +34,5 @@ TYPE=$(echo $TYPE | xargs)
 MASS=$(echo $MASS | xargs)
 MELT=$(echo $MELT | xargs)
 BOIL=$(echo $BOIL | xargs)
+
+echo "The element with atomic number $ATOMIC_NUMBER is $NAME ($SYMBOL). It's a $TYPE, with a mass of $MASS amu. $NAME has a melting point of $MELT celsius and a boiling point of $BOIL celsius."
