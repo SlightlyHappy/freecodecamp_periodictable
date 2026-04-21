79d875f6c4577fa8851e584eddb2379287d2cfac
SlightlyHappy <104701274+SlightlyHappy@users.noreply.github.com>
2026-04-21 12:47:32 -0400

    refactor: please p

--- element.sh
+++ element.sh
@@ -23,3 +23,14 @@ fi
 IFS="|" read ATOMIC_NUMBER SYMBOL NAME <<< "$ELEMENT"
 
 PROPERTIES=$($PSQL "SELECT atomic_mass, melting_point_celsius, boiling_point_celsius, type_id FROM properties WHERE atomic_number=$ATOMIC_NUMBER")
+
+IFS="|" read MASS MELT BOIL TYPE_ID <<< "$PROPERTIES"
+
+TYPE=$($PSQL "SELECT type FROM types WHERE type_id=$TYPE_ID")
+
+NAME=$(echo $NAME | xargs)
+SYMBOL=$(echo $SYMBOL | xargs)
+TYPE=$(echo $TYPE | xargs)
+MASS=$(echo $MASS | xargs)
+MELT=$(echo $MELT | xargs)
+BOIL=$(echo $BOIL | xargs)
